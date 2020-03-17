dea5e1d4e4a6ce8237920d94e533034cb17648ed
Tim Player <tplayer@hmc.edu>
2020-03-17 12:53:57 +0000

    debugged test_as_5048.py

--- test_as5048b.py
+++ test_as5048b.py
@@ -8,11 +8,15 @@ import time
 from datetime import datetime
 
 bus = smbus.SMBus(1)
-RE_ADDRESS_LIST = [0x40, 0x41]
+#RE_ADDRESS_LIST = [0x40, 0x41]
+RE_ADDRESS_LIST = [0x40]
 RE_ZEROMSB_REG = 0x16  # Zero, most significant byte
 RE_ZEROLSB_REG = 0x17  # Zero, least significant byte
+RE_MAGNMSB_REG = 0xFC  # Magnitude, most significant byte
+RE_MAGNLSB_REG = 0xFD  # Magnitude, least significant byte
 RE_ANGLEMSB_REG = 0xFE  # Angle, most significant byte
 RE_ANGLELSB_REG = 0xFF  # Angle, least significant byte
+
 
 # sets initial position to zero
 for RE_ADDRESS in RE_ADDRESS_LIST:
@@ -26,13 +30,18 @@ for RE_ADDRESS in RE_ADDRESS_LIST:
 # polling
 while True:
     startTime = time.time()
-    ANG_M1 = bus.read_byte_data(0x40, 0x16)
-    ANG_L1 = bus.read_byte_data(0x40, 0x17)
-    print(time.time()-startTime)
-    startTime = time.time()
-    ANG_M2 = bus.read_byte_data(0x41, 0x16)
-    ANG_L2 = bus.read_byte_data(0x41, 0x17)
-    print(time.time()-startTime)
-    ANG1 = (ANG_M1 << 6)+(ANG_L1 & 0x3f)
-    ANG2 = (ANG_M2 << 6)+(ANG_L2 & 0x3f)
+    ANG_M1 = bus.read_byte_data(0x40, RE_ANGLEMSB_REG)
+    ANG_L1 = bus.read_byte_data(0x40, RE_ANGLELSB_REG)
+    # bit shifting to access 14-bit value. Docs p. 25
+    ANG1 = (ANG_M1 << 6)+(ANG_L1 & 0x3f) 
+    ANG1 = ANG1 * 360.0 / 16384.0
+    
+    MAG_M1 = bus.read_byte_data(0x40, RE_MAGNMSB_REG)
+    MAG_L1 = bus.read_byte_data(0x40, RE_MAGNLSB_REG)
+    # bit shifting to access 14-bit value. Docs p. 25
+    MAG1 = (MAG_M1 << 6)+(MAG_L1 & 0x3f) 
+
+    print("Angle: %7f Magnitude: %7d" %(ANG1, MAG1))
+    #print("Time taken: " + str(time.time()-startTime))
+    
     time.sleep(0.1)
