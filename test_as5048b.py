79cfd1e4f1ac2e2ad741f3f2fd12dfc57c083a0c
Tim Player <tplayer@hmc.edu>
2020-03-27 02:55:16 +0000

    added test_nrf24 and modified test_as5048b for variance

--- test_as5048b.py
+++ test_as5048b.py
@@ -27,21 +27,33 @@ for RE_ADDRESS in RE_ADDRESS_LIST:
     bus.write_byte_data(RE_ADDRESS, RE_ZEROMSB_REG, ANG_M)
     bus.write_byte_data(RE_ADDRESS, RE_ZEROLSB_REG, ANG_L)
 
+angs = [0]*100
+n = 0
 # polling
 while True:
+    n += 1
+
     startTime = time.time()
+
     ANG_M1 = bus.read_byte_data(0x40, RE_ANGLEMSB_REG)
     ANG_L1 = bus.read_byte_data(0x40, RE_ANGLELSB_REG)
     # bit shifting to access 14-bit value. Docs p. 25
     ANG1 = (ANG_M1 << 6)+(ANG_L1 & 0x3f) 
     ANG1 = ANG1 * 360.0 / 16384.0
+
+    angs = [ANG1] + angs[:-1] # Add new measurement to front of list.
+    # Scoot the last element off the list.
     
+    mean = sum(angs) / len(angs) 
+    variance = sum([((x - mean) ** 2) for x in angs]) / len(angs) 
+
     MAG_M1 = bus.read_byte_data(0x40, RE_MAGNMSB_REG)
     MAG_L1 = bus.read_byte_data(0x40, RE_MAGNLSB_REG)
     # bit shifting to access 14-bit value. Docs p. 25
     MAG1 = (MAG_M1 << 6)+(MAG_L1 & 0x3f) 
 
-    print("Angle: %7f Magnitude: %7d" %(ANG1, MAG1))
-    #print("Time taken: " + str(time.time()-startTime))
+    if n % 10 == 0:
+        print("Angle: %7f Magnitude: %7d Variance: %7f" %(ANG1, MAG1, variance))
     
-    time.sleep(0.1)
+    time.sleep(0.01)
+
